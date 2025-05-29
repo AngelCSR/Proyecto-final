062b61f54303f9a2aeac5cfd01b69b12df41ba1e
javier <javiermoralessimon7@gmail.com>
2025-05-29 21:41:58 +0200

    hago javascript #3

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const capitalInput = document.getElementById('capital');
+
+    const interesInput = document.getElementById('interes');
+
+    const aniosInput = document.getElementById('anios');
+
+    const calcularBtn = document.getElementById('calcularBtn');
+
+    const resultadoDiv = document.getElementById('resultado');
+
+
+    calcularBtn.addEventListener('click', calcularHipoteca);
+})
